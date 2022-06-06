4ca7a6020e6b081b0472a2346e20a0049af26338
natenadze1993 <g.natenadze@myhome.ge>
2022-06-06 19:28:48 +0400

    123

--- food-order-app/src/Components/Header/Header.js
+++ food-order-app/src/Components/Header/Header.js
@@ -2,6 +2,7 @@ import { Fragment } from 'react';
 import styles from './Header.module.css';
 
 import HeaderCartButton from '../UI/HeaderCartButton';
+import CartIcon from '../UI/CartIcon';
 
 const Header = () => {
 
@@ -9,7 +10,12 @@ const Header = () => {
         <Fragment>
             <div className={styles.header}>
                 <h1>React Meals</h1>
-                <HeaderCartButton>Button</HeaderCartButton>
+                <HeaderCartButton>
+                    <div className='icon'>
+                        <CartIcon />
+                    </div>
+
+                </HeaderCartButton>
             </div>
             <div className={styles['main-image']}>
                 <img src={require('./meals.jpg').default} />
